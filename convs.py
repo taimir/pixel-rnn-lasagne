Fix masking

convs.py
@@ -1,7 +1,7 @@
 import theano.tensor as T
 import lasagne
 
-from masks import mask_a, mask_b
+from masks import mask
 
 
 class MaskedConv2D(lasagne.layers.Conv2DLayer):
@@ -9,7 +9,7 @@ class MaskedConv2D(lasagne.layers.Conv2DLayer):
     Note: the layer does not have a non-linearity on top of it
     """
 
-    def __init__(self, incoming, num_filters, filter_size, mask="a", stride=(1, 1), untie_biases=False,
+    def __init__(self, incoming, num_filters, filter_size, mask_type, n_colors, stride=(1, 1), untie_biases=False,
                  W=lasagne.init.GlorotUniform(),
                  b=lasagne.init.Constant(0.),
                  nonlinearity=lasagne.nonlinearities.identity, **kwargs):
@@ -17,13 +17,11 @@ class MaskedConv2D(lasagne.layers.Conv2DLayer):
                                            pad="same", untie_biases=untie_biases, W=W, b=b,
                                            nonlinearity=nonlinearity,
                                            convolution=T.nnet.conv2d, **kwargs)
-        self.mask = mask
+        self.mask_type = mask_type
+        self.n_colors = n_colors
 
     def convolve(self, input, **kwargs):
-        if self.mask == "a":
-            W = mask_a(self.W)
-        elif self.mask == "b":
-            W = mask_b(self.W)
+        W = mask(self.W, n_colors=self.n_colors, type=self.mask_type)
         conved = self.convolution(input, W,
                                   self.input_shape, self.get_W_shape(),
                                   subsample=self.stride,
@@ -38,9 +36,10 @@ if __name__ == "__main__":
 
     input = T.tensor4("filter")
 
-    input_layer = lasagne.layers.InputLayer(input_var=input, shape=(None, 3, 9, 9))
-    network = MaskedConv2D(incoming=input_layer, num_filters=4, filter_size=(3, 3))
+    input_layer = lasagne.layers.InputLayer(input_var=input, shape=(None, 1, 3, 3))
+    network = MaskedConv2D(incoming=input_layer, num_filters=4, filter_size=(3, 3), mask_type="a", n_colors=1)
     output = lasagne.layers.get_output(network)
     f = theano.function(inputs=[input], outputs=output)
-    convolved = f(np.ones((1, 3, 9, 9), dtype=np.float32))
+    test_image = np.ones((1, 1, 3, 3), dtype=np.float32)
+    convolved = f(test_image)
     print(convolved)

masks.py
@@ -7,83 +7,58 @@ import logging
 log.basicConfig(level=logging.DEBUG)
 
 
-def mask_spatial_context(filter):
-    filter_side = filter.shape[-1]
-    half = filter_side // 2
+def mask_spatial_context(filter, center):
     # zero out context connections
-    filter = T.set_subtensor(filter[:, :, half + 1:, :], 0)
-    filter = T.set_subtensor(filter[:, :, half, half + 1:], 0)
+    filter = T.set_subtensor(filter[:, :, center[0] + 1:, :], 0)
+    filter = T.set_subtensor(filter[:, :, center[0], center[1] + 1:], 0)
     return filter
 
 
-def mask_a(filter):
+def mask(filter, n_colors, type):
     """
     :param filter: 4d symbolic filter
             (output_channels x input_channels x height x width)
     :return: masked filter
     """
 
-    filter = mask_spatial_context(filter)
+    center = (filter.shape[-2] // 2, filter.shape[-1] // 2)
+    filter = mask_spatial_context(filter, center)
 
     # mask the connections between input channels and output channels
-    # order of channels is RGB
-    out_chan_part = filter.shape[0] // 3
-    in_chan_part = filter.shape[1] // 3
-    center = filter.shape[-1] // 2
+    out_part_size = filter.shape[0] // n_colors
+    in_part_size = filter.shape[1] // n_colors
 
-    R_out = filter.shape[0] - 2*out_chan_part
-    RG_out = R_out + out_chan_part
-    RGB_out = RG_out + out_chan_part
+    # consider the residuals in case the out- or in-channel shapes
+    # are not divisible by the number of colors
+    out_residual = filter.shape[0] % n_colors
+    in_residual = filter.shape[1] % n_colors
 
-    R_in = filter.shape[1] - 2*in_chan_part
-    RG_in = R_in + in_chan_part
+    assert (type == "a" or type == "b"), "Mask type can be only 'a' or 'b'"
 
-    # R_out does not see any of the input channels
-    filter = T.set_subtensor(filter[:R_out, :, center, center], 0)
-    # G_out does not see anything after the R input channel
-    filter = T.set_subtensor(filter[R_out:RG_out, R_in:, center, center], 0)
-    # B_out does not see anything after the R and G input channels
-    filter = T.set_subtensor(filter[RG_out:RGB_out, RG_in:, center, center], 0)
-
-    return filter
-
-
-def mask_b(filter):
-    """
-        :param filter: 4d symbolic filter
-                (output_channels x input_channels x height x width)
-        :return: masked filter
-        """
-
-    filter = mask_spatial_context(filter)
-
-    # mask the connections between input channels and output channels
-    # order of channels is RGB
-    out_chan_part = filter.shape[0] // 3
-    in_chan_part = filter.shape[1] // 3
-    center = filter.shape[-1] // 2
-
-    R_out = filter.shape[0] - 2*out_chan_part
-    RG_out = R_out + out_chan_part
-
-    R_in = filter.shape[1] - 2*in_chan_part
-    RG_in = R_in + in_chan_part
-
-    # R_out does not see anything after R_in
-    filter = T.set_subtensor(filter[:R_out, R_in:, center, center], 0)
-    # G_out does not see anything after the G_in
-    filter = T.set_subtensor(filter[R_out:RG_out, RG_in:, center, center], 0)
-    # B_out does sees it all, no need to zero it out
+    curr_out_part_pos = 0
+    curr_in_part_pos = 0
+    # Count the residuals to the part of the first color.
+    next_out_part_pos = out_residual + out_part_size
+    next_in_part_pos = in_residual + in_part_size
+    for i in range(n_colors):
+        masked_subtensor = filter[curr_out_part_pos:next_out_part_pos,
+                           curr_in_part_pos if type == "a" else next_in_part_pos:,
+                           center[0], center[1]]
+        curr_in_part_pos = next_in_part_pos
+        curr_out_part_pos = next_out_part_pos
+        next_in_part_pos += in_part_size
+        next_out_part_pos += out_part_size
+        filter = T.set_subtensor(masked_subtensor, 0)
 
     return filter
 
 
 if __name__ == "__main__":
     input = T.tensor4("filter")
-    output = mask_a(input)
+    output = mask(input, n_colors=3, type="b")
     f = theano.function(inputs=[input], outputs=output)
 
-    masked = f(np.ones((3, 3, 3, 3), dtype=np.float32))
+    masked = f(np.ones((4, 4, 3, 3), dtype=np.float32))
 
     log.info("red out")
     log.info(masked[0])